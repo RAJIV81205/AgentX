5d9911d24a04478ce63b7a2f493f80d3946e0289
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-03-09 23:50:57 +0530

    errors fixed + token verification added

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,17 +1,36 @@
 import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
 import LoginForm from "../components/Login";
-
-import { useState } from "react";
 import SignupForm from "../components/Signup-Form";
 
 function Auth() {
   const navigate = useNavigate();
   const [signup, setSignup] = useState(false);
   const token = localStorage.getItem("token");
+  const url = import.meta.env.VITE_BACKEND;
 
-  if (token) {
-    navigate("/dashboard");
-  }
+  useEffect(() => {
+    const verifyToken = async () => {
+      if (token) {
+        try {
+          const response = await fetch(`${url}/verify-token`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ token }),
+          });
+          const data = await response.json();
+
+          if (data.message !== "Invalid token") {
+            navigate("/dashboard");
+          }
+        } catch (error) {
+          console.error("Token verification failed:", error);
+        }
+      }
+    };
+
+    verifyToken();
+  }, [navigate, token]);
 
   return (
     <div className="w-full flex justify-center items-center flex-col min-h-dvh bg-gray-100 py-10">
